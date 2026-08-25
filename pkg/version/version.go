9a6e12677e8dc98d7ae0a99905ea80c914e6a144
Marawan morsy <m.2396@nu.edu.eg>
2026-08-26 00:36:42 +0300

    release: make version metadata injectable at build time

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -1,4 +1,8 @@
-// Package version defines the ANPU release version used by CLI and reports.
+// Package version defines the ANPU release version used by the CLI and reports.
 package version
 
-const Version = "0.3.1"
+// Version is the release version. GoReleaser overrides it at build time.
+var Version = "0.3.1"
+
+// Commit is the source commit used to build the binary. GoReleaser overrides it at build time.
+var Commit = "unknown"
